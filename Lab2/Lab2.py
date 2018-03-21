a644fef30e60be2240630cd1b40dc18cb7052107
LordOfNightmares <macbobXD@gmail.com>
2018-03-21 20:03:29 +0200

    test
    test

--- Lab2/Lab2.py
+++ Lab2/Lab2.py
@@ -1,20 +1,11 @@
-import requests
+from pprint import pprint
 from bs4 import BeautifulSoup
 import requests
-from contextlib import closing
-import csv
-import codecs
 import json
-from pprint import pprint
 import time
 from threading import Thread as T, Lock as L
-import _thread
-from pprint import pprint
 
-link = "https://evil-legacy-service.herokuapp.com/api/v101"
-url = [link + "/categories/", link + "/orders/"]
-headers = {"Accept": "text/csv",
-           "X-API-KEY": "55193451-1409-4729-9cd4-7c65d63b8e76"}
+url = "https://desolate-ravine-43301.herokuapp.com"
 
 
 class BC:
@@ -28,61 +19,84 @@ class BC:
     UNDERLINE = '\033[4m'
 
 
-'''
-class CountdownThread(T):
-    def __init__(self, count):
+class Threads(T):
+    def __init__(self, url, headers):
         T.__init__(self)
-        self.count = count
+        self.url, self.headers = url, headers
 
     def run(self):
-        while self.count > 0:
-            current_thread = self.name[len("Thread-")::]
-            with L():
-                if current_thread == '1':
-                    print(BC.OKBLUE, "1-Countdown", self.count, self.count)
-                if current_thread == '2':
-                    print(BC.WARNING, "2-Countdown", self.count, self.count)
-                self.count -= 1
+        def output(device_id, sensor, value):
+            device_id, sensor, value = device_id, int(sensor), float(value)
+            if sensor == 0:
+                print(BC.OKGREEN, "Temperature:")
+            if sensor == 1:
+                print(BC.OKGREEN, "Humidity:")
+            if sensor == 2:
+                print(BC.OKGREEN, "Alien Presence:")
+            if sensor == 3:
+                print(BC.OKGREEN, "Dark Matter")
+            if sensor == 4:
+                print(BC.OKGREEN, "Ghost Presence:")
+            if sensor >= 5:
+                print(BC.OKBLUE, "Does this Sensor even exist:")
+            # ---------------------
+            print("Device", device_id, "-", value, sep=' ', end='', flush=True)
+            # ---------------------
+            if value > 0 and sensor == 3:
+                print(BC.WARNING, "(Is 'CERN' particle accelerator turned off?)")
+            if value == 0 and sensor == 2:
+                print("(No aliens detected)")
+            if value > 0 and sensor == 2:
+                print(BC.FAIL, "(RUN - ALIEN LIFE DETECTED)")
+            if value > 0 and sensor == 4:
+                print(BC.FAIL, "(RIP - GHOSTS DETECTED)")
+            print()
 
-                # time.sleep(.1)
-        return
-'''
+        def parsing(content_type, text):
+            if content_type == "Application/xml":
+                soup = BeautifulSoup(text, 'html.parser')
+                output(soup.device['id'], soup.type.string, soup.value.string)
+            elif content_type == "Application/json":
+                j_file = json.loads(text)
+                output(j_file['device_id'], j_file['sensor_type'], j_file['value'])
+
+            elif content_type == "text/csv":
+                text = [s.split(",") for s in text.splitlines()]
+                for i in range(len(text)):
+                    if i != 0:
+                        output(text[i][0], text[i][1], text[i][2])
+            else:
+                print(text)
+
+        with L():
+            start = time.time()
+            response = requests.get(self.url, headers=self.headers)
+            roundtrip = time.time() - start
+            print(BC.HEADER, "\n", self.name, "Statuscode",
+                  response.status_code, round(roundtrip, 4), "s\nUrl:", self.url)
+            parsing(response.headers["Content-Type"], response.text)
 
 
-#
-
-def reading(url, headers):
+def reading(url):
     # link = url + key
-    response = requests.get(url, headers=headers)
+    response = requests.post(url)
     print("Status code", response.status_code)
-    with closing(requests.get(url, headers=headers)) as r:
-        rcsv = csv.reader(codecs.iterdecode(r.iter_lines(), 'utf-8'), delimiter=',', quotechar='"')
-        next(rcsv, None)
-        for row in rcsv:
-            print(str(rcsv.line_num) + ' ' + str(row))
-            # jfile = json.dumps([{key, val} for key in rcsv[0] for val in rcsv[1::]])
-    # pprint(rcsv)
+    headers = {"Session": response.headers['Session']}
+    urls = path(response.json())
+    Tr = []
+
+    for i in range(len(urls)):
+        Tr.append(Threads(urls[i], headers))
+    for i in range(len(urls)):
+        Tr[i].start()
     return
 
 
-def print_time(threadName, delay=.30):
-        time.sleep(delay)
-        if int(threadName[len("Thread-")::]) == 1:
-            with L():
-                print(BC.OKBLUE, "%s: %s" % (threadName, time.ctime(time.time())))
-                reading(url[0], headers)
-        if int(threadName[len("Thread-")::]) == 2:
-            with L():
-                print(BC.WARNING, "%s: %s" % (threadName, time.ctime(time.time())))
-                reading(url[1], headers)
+def path(text):
+    path = []
+    for i in range(len(text)):
+        path.append(url + text[i]['path'])
+    return path
 
 
-# Create two threads as follows
-try:
-    _thread.start_new_thread(print_time, ("Thread-1",))
-    _thread.start_new_thread(print_time, ("Thread-2",))
-except:
-    print("Error: unable to start thread")
-
-while 1:
-    pass
+reading(url)
